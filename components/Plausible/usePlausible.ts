6e03618224c0c740fa36ce579da1e02208592958
Merott Movahedi <merott@merott.com>
2022-03-14 12:43:31 +0000

    upgrade next-plausible

--- components/Plausible/usePlausible.ts
+++ components/Plausible/usePlausible.ts
@@ -1,13 +1,10 @@
 // eslint-disable-next-line no-restricted-imports
 import { usePlausible as useNextPlausible } from 'next-plausible'
-import { useCallback } from 'react'
 
 export function usePlausible() {
-  const plausible = useNextPlausible()
-
-  // disabling deps rule because the upstream implementation never changes
-  // but it isn't set up with useCallback, so every time it's a new function
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  return useCallback(plausible, [])
+  return useNextPlausible<{
+    'subscribe': never
+    'view garden explainer': never
+    'view subscription panel': never
+  }>()
 }
